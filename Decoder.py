ce6b423e7894e5af21f94f978fde15886021f437
NEERAJ KUMAR KHANDELWAL <nkkhandewal24@gmail.com>
2024-05-15 17:19:19 +0530

    Create Decoder.py

---
+++ Decoder.py
@@ -0,0 +1,35 @@
+from utils import *
+import torch
+import os
+from os.path import exists
+import torch.nn as nn
+from torch.nn.functional import log_softmax, pad
+import math
+import copy
+
+class Decoder(nn.Module):
+    def __init__(self, layer, N):
+        super(Decoder, self).__init__()
+        self.layers = clones(layer, N)
+        self.norm = LayerNorm(layer.size)
+        
+    def forward(self, x, memory, src_mask, tgt_mask):
+        for layer in self.layers:
+            x = layer(x, memory, src_mask, tgt_mask)
+        return self.norm(x)
+
+
+class DecoderLayer(nn.Module):
+    def __init__(self, size, self_attn, src_attn, feed_forward, dropout):
+        super(DecoderLayer, self).__init__()
+        self.size = size
+        self.self_attn = self_attn
+        self.src_attn = src_attn
+        self.feed_forward = feed_forward
+        self.sublayer = clones(SubLayerConnection(size, dropout), 3)
+    def forward(self, x, memory, src_mask, tgt_mask):
+        m = memory
+        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, tgt_mask))
+        x = self.sublayer[1](x, lambda x: self.src_attn(x, m, m, src_mask))
+        return self.sublayer[2](x, self.feed_forward)
+      
