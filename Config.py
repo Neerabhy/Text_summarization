53fa6badf9b344b340da133247200be65643cd8e
NEERAJ KUMAR KHANDELWAL <nkkhandewal24@gmail.com>
2024-05-15 21:58:31 +0530

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,21 @@
+
+config = {
+        "batch_size": 64,
+        "distributed": False,
+        "num_epochs": 10,
+        "accum_iter": 10,
+        "base_lr": 0.005,
+        "max_padding": 72,
+        "warmup": 3000,
+        "file_prefix": "multi30k_model_",
+    }
+d_model = 512
+device = torch.device("cuda" if torch.cuda.is_available else 'cpu')
+model = make_model(len(vocab_src), len(vocab_tgt), N = 6)
+optimizer = torch.optim.Adam(model.parameters(), lr = config["base_lr"],
+                                betas = (0.9, 0.98), eps = 1e-9)
+lr_scheduler = LambdaLR(optimizer = optimizer,
+                        lr_lambda = lambda step :rate(step, d_model, factor = 1, warmup = config["warmup"]),)
+
+model.to(device)
+train_model(model, vocab_src, vocab_tgt, spacy_en, spacy_hi, config, optimizer, lr_scheduler)
